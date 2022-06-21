4b4b1c1489684376cb68ce1eaf42395beec568c2
markkravic <hbklksndr@protonmail.com>
2022-06-21 13:47:51 +0300

    upd

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-const fs = require("fs")
+// const fs = require("fs")
+const fs = require("fs/promises")
 const path = require("path")
 
-const sortFolder = (read, gender, write) => {
-    fs.readdir(path.join(__dirname, read), (err, files) => {
-        if (err) return console.log(err)
+// const sortFolder = (read, gender, write) => {
+//     fs.readdir(path.join(__dirname, read), (err, files) => {
+//         if (err) return console.log(err)
+//
+//         files.forEach((file) => {
+//             const folderPath = path.join(__dirname, read, file)
+//             fs.readFile(folderPath, (err, data) => {
+//                 if (err) console.log(err)
+//
+//                 const user = JSON.parse(data.toString())
+//
+//                 if (user.gender === gender) {
+//                     fs.rename(folderPath, path.join(__dirname, write, file), (err) => {
+//                         if (err) console.log(err)
+//                     })
+//                 }
+//             })
+//         })
+//     })
+// }
+//
+// sortFolder('girls', 'male', 'boys')
+// sortFolder('boys', 'female', 'girls')
 
-        files.forEach((file) => {
+const sortFolder = async (read, gender, write) => {
+    try {
+        const files = await fs.readdir(path.join(__dirname, read))
+
+
+        for (const file of files) {
             const folderPath = path.join(__dirname, read, file)
-            fs.readFile(folderPath, (err, data) => {
-                if (err) console.log(err)
+            const data = await fs.readFile(folderPath)
 
-                const user = JSON.parse(data.toString())
+            const user = JSON.parse(data.toString())
 
-                if (user.gender === gender) {
-                    fs.rename(folderPath, path.join(__dirname, write, file), (err) => {
-                        if (err) console.log(err)
-                    })
-                }
-            })
-        })
-    })
+            if (user.gender === gender) {
+                await fs.rename(folderPath, path.join(__dirname, write, file))
+            }
+        }
+    } catch (e) {
+        console.error(e)
+    }
 }
+
 
 sortFolder('girls', 'male', 'boys')
 sortFolder('boys', 'female', 'girls')
